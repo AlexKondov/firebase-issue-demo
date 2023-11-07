c7da8e99d4ae46bff875ba38b218cd8132367aea
AlexKondov <alexkondov38@gmail.com>
2023-11-08 01:05:25 +0200

    return more commented out code

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-// import Navigation from "../../components/Navigation";
+import Navigation from "../../components/Navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <Navigation /> */}
+      <Navigation />
       <main className="flex flex-col py-2 mx-auto max-w-3xl px-4 md:px-0">
         {children}
       </main>
--- app/tavern/layout.tsx
+++ app/tavern/layout.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-// import Navigation from "../../components/Navigation";
+import Navigation from "../../components/Navigation";
 import QueryProvider from "../../components/QueryProvider";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <Navigation /> */}
+      <Navigation />
       <main className="flex flex-col py-2 mx-auto max-w-3xl px-4 md:px-0 my-10">
         <QueryProvider>{children}</QueryProvider>
       </main>

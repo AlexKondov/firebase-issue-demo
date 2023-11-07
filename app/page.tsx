01d052cf12453aa4736061274298ff1ed889f08c
AlexKondov <alexkondov38@gmail.com>
2023-11-07 22:29:45 +0200

    add auth page to test firebase error

--- app/page.tsx
+++ app/page.tsx
@@ -1,118 +1,140 @@
 "use client";
 
-import Image from 'next/image'
-import { auth } from '@/lib/firebase'
+import { useEffect, useRef, useState } from "react";
+import { EmailAuthProvider, User } from "@firebase/auth";
+import { useRouter } from "next/navigation";
+import "firebaseui/dist/firebaseui.css";
+import { auth } from "@/lib/firebase";
 
-console.log(auth)
+function getRandomTavernLocation() {
+  const tavernLocations = [
+    "table by the small window",
+    "table by the fireplace",
+    "table in the back",
+    "chair at the bar",
+  ];
 
-export default function Home() {
+  const location =
+    tavernLocations[Math.floor(Math.random() * tavernLocations.length)];
+
+  return location;
+}
+
+function getButtonText(isLoading: boolean, available: boolean) {
+  if (isLoading) {
+    return "Checking...";
+  }
+
+  if (typeof available === "boolean" && !available) {
+    return "Name is taken. Try another one!";
+  }
+
+  return "Save name";
+}
+
+export default function Auth() {
+  const router = useRouter();
+  const [firebaseui, setFirebaseui] = useState<any>(null);
+  const [user, setUser] = useState<null | User>(null);
+  const [showNameForm, setShowNameForm] = useState(false);
+  const [name, setName] = useState("");
+  // TODO: we put this in state to retain the initial value
+  const [location] = useState(() => getRandomTavernLocation());
+  const elementRef = useRef(null);
+
+  const uiConfig = {
+    signInFlow: "popup",
+    callbacks: {
+      signInSuccessWithAuthResult: (authResult: any) => {
+        if (authResult.additionalUserInfo.isNewUser) {
+          setShowNameForm(true);
+          return;
+        }
+
+        router.push("/tavern");
+      },
+    },
+    signInOptions: [
+      {
+        provider: EmailAuthProvider.PROVIDER_ID,
+        requireDisplayName: false,
+      },
+    ],
+  };
+
+  useEffect(() => {
+    // Firebase UI only works on the Client. So we're loading the package only after
+    // the component has mounted, so that this works when doing server-side rendering.
+    setFirebaseui(require("firebaseui"));
+  }, []);
+
+  useEffect(() => {
+    if (firebaseui === null) {
+      return;
+    }
+
+    // Get or Create a firebaseUI instance.
+    const firebaseUiWidget =
+      firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
+
+    if (uiConfig.signInFlow === "popup") {
+      firebaseUiWidget.reset();
+    }
+
+    // We track the auth state to reset firebaseUi if the user signs out.
+    let unregisterAuthObserver = () => {};
+
+    unregisterAuthObserver = auth.onAuthStateChanged((firebaseUser) => {
+      if (!firebaseUser && user) {
+        firebaseUiWidget.reset();
+      }
+      setUser(firebaseUser);
+    });
+
+    // Render the firebaseUI Widget.
+    // @ts-ignore
+    firebaseUiWidget.start(elementRef.current, uiConfig);
+
+    return () => {
+      unregisterAuthObserver();
+      firebaseUiWidget.reset();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [firebaseui, uiConfig]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col py-2 mx-auto max-w-2xl px-4 md:px-0">
+      <div className="grid mt-20">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-6xl mb-4 text-center w-full">
+            {showNameForm ? "One more thing..." : "Glad you made it!"}
+          </h1>
+          {/** Interesting example of suppressing hydration warning */}
+          <p className="text-xl mb-10 text-center" suppressHydrationWarning>
+            {showNameForm
+              ? `How should we call you here? This name will be visible to the other patrons.`
+              : `Sit on a ${location} and join the conversation.`}
+          </p>
+          {showNameForm ? (
+            <>
+              <input
+                className="w-full text-center mt-16 mb-4 text-xl focus:outline-none border rounded border-black py-2"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <button
+                className="block cursor-pointer w-full bg-black text-white py-2 text-xl rounded shadow-solid transition-all"
+              >
+                test
+              </button>
+            </>
+          ) : null}
+          <div
+            className={`auth-form w-full ${showNameForm ? "invisible" : ""}`}
+            ref={elementRef}
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
